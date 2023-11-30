dd2b6c91a39f86e9a6b6a65716d6494af6490231
Athena <bqh1402003@gmail.com>
2023-11-30 09:46:49 +0700

    discover: update read all file in directory

--- server/discover.py
+++ server/discover.py
@@ -1,16 +1,18 @@
+import os
 import sys
 
 # List published filenames from host named hostname
 def discover(hostname):
     files = []
-    with open("peers/" + hostname, "r") as f:
-        while True:
-            data = f.readline()
+    for filename in os.listdir("peers/" + hostname):
+        with open("peers/" + hostname + "/" + filename, "r") as f:
+            while True:
+                data = f.readline()
 
-            #end of file
-            if len(data) == 0:
-                break
-            files.append(data.split(' | ')[1])
+                #end of file
+                if len(data) == 0:
+                    break
+                files.append(data.split(' | ')[1])
     data = "".join(files)
     print(data)
     return data
@@ -19,14 +21,13 @@ def main():
     # Syntax check
     if len(sys.argv) != 2:
         print("Usage: python discover.py hostname")
+        return
         
     # Get hostname (address) from argument | [0] is ALWAYS script name
     hostname = sys.argv[1]
 
     # Call discover function
     discover(hostname)
-
-
 
 if __name__ == '__main__':
     main()
